ab3e41884215ada9b44d5c2412db76a52c065b16
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2014-07-29 15:52:30 +1200

    shuffling things around a bit to keep TP and DBS stuff separate

--- common.r
+++ common.r
@@ -1,3 +1,4 @@
+
 spread = function (v, m = 0, M = 1) 
 {
    v <- v - min(v)
@@ -9,20 +10,9 @@ spread = function (v, m = 0, M = 1)
 
 mostFrequent = function(vec, w=NA)
 {
-   if(is.na(w)) w <- rep(1, length(vec)) # what is this supposed to do? w isn't used anywhere else in the function!
+   #if(is.na(w)) w <- rep(1, length(vec)) # what is this supposed to do? w isn't used anywhere else in the function!
    tvec <- table(vec)
    nvec <- as.vector(tvec[as.factor(vec)])
    return(which.is.max(nvec))
 }
 
-netToAdjacency = function(net){
-    x <- as.factor(net[,1])
-    y <- as.factor(net[,2])
-    z <- net[,3]
-    adj <- matrix(0,
-                  nrow=nlevels(x),
-                  ncol=nlevels(y),
-                  dimnames=list(levels(x), levels(y)))
-    adj[cbind(x, y)] <- z
-    adj
-}
